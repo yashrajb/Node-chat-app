5f93cf6f7ff317b5df2eb28dbdcd58b20921559d
Yashraj Basan <basanyash627@gmail.com>
2018-07-04 23:24:55 +0530

    Emiting events

--- server/server.js
+++ server/server.js
@@ -1,14 +1,31 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-
+const http = require('http');
+const socketIO = require('socket.io');
 var port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,'../public');
+const server = http.createServer(app);
+var io = socketIO(server);
 app.use(express.static(publicPath));
 
-
-app.listen(port,() => {
-
-console.log(`server is started at the ${port}`);
-
+io.on('connection',function(socket){
+    console.log('new connetion');
+    socket.on('disconnect',function(){
+        console.log('disconnected');
+    });
+   socket.emit('newMessage',{
+       from:'basanyash@gmail.com',
+       text:'Hello bro!',
+       createdAt:new Date()
+   });
+   socket.on('createMessage',function(msg){
+        console.log(msg);
+   })
+    socket.on('sendEmail',function(email){
+        console.log(email);
+    })
+});
+server.listen(port,function(){
+    console.log(`server is started at the ${port}`);
 })
