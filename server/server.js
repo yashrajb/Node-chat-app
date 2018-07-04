5690f08b4d2382933c1938ccbfa6cd66278da230
Yashraj Basan <basanyash627@gmail.com>
2018-07-04 06:56:40 +0530

    solved port problem

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-const port = 3000 || process.env.port;
+const port = 3000 || process.env.PORT;
 const publicPath = path.join(__dirname,'../public');
 app.use(express.static(publicPath));
 
