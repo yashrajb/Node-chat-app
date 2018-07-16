44e0ebdebe78466ac4c5fa901178f9361b9fdbbd
Yashraj Basan <basanyash627@gmail.com>
2018-07-16 17:13:17 +0530

    added timestamps in chat

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,28 +1,19 @@
 const axios = require('axios');
+const moment = require('moment');
 var generateMessages = function(from,text){
     return {
         from,
         text,
-        createdAt:new Date().getTime()
+        createdAt:moment().valueOf()
     }
 }
 
 var generateLocationMessages = function(from,latitude,longitude){
- return {
-     from,
-     url:`https://www.google.com/maps?q=${latitude},${longitude}`,
-     createdAt:new Date().getTime()
- }
-}
-
-var generateWeatherMessages =  function(from,latitude,longitude){
-    var res = {};
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=bd9d472d8109b5b0d75782551b2f0890`)
-    .then(function(response){
-        res = response.data.weather;
-    });
-
-    console.log(res);
+    return {
+        from,
+        url:`https://www.google.com/maps?q=${latitude},${longitude}`,
+        createdAt:moment().valueOf()
+    }
 }
 
 module.exports = {
