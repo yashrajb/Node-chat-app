c7ae592a24daea683ee7644c137cce5b2177e61a
Yashraj Basan <basanyash627@gmail.com>
2018-07-16 17:24:38 +0530

    removed comments

--- public/js/moment.js
+++ public/js/moment.js
@@ -1,8 +1,3 @@
-//! moment.js
-//! version : 2.15.1
-//! authors : Tim Wood, Iskren Chernev, Moment.js contributors
-//! license : MIT
-//! momentjs.com
 
 ;(function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
